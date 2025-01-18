aebe7e606049bf99fea866be4badd592157d48db
ArisSpn_9 <arissopian0909@gmail.com>
2025-01-19 05:27:41 +0700

    coba

---
+++ api/back.py
@@ -0,0 +1,18 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='../src')
+
+@app.route('/')
+def serve_index():
+    return send_from_directory(app.static_folder, 'dashboard.html')
+
+@app.route('/src/<path:path>')
+def serve_src(path):
+    return send_from_directory(app.static_folder, path)
+
+@app.route('/img/<path:path>')
+def serve_img(path):
+    return send_from_directory('../img', path)
+
+if __name__ == "__main__":
+    app.run(debug=True)
