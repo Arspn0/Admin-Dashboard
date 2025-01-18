65c263123636ca36b68fa0d671c4d158c904438d
ArisSpn_9 <arissopian0909@gmail.com>
2025-01-19 05:14:15 +0700

    ubah tros

--- api/app.py
+++ api/app.py
@@ -1,18 +1,20 @@
-from flask import Flask, send_from_directory
+# api/app.py
+from flask import Flask, send_from_directory, send_file
+import os
 
-app = Flask(__name__, static_folder='../src')
+app = Flask(__name__)
 
 @app.route('/')
 def serve_index():
-    return send_from_directory(app.static_folder, 'dashboard.html')
+    return send_file('../src/dashboard.html')
 
 @app.route('/src/<path:path>')
 def serve_src(path):
-    return send_from_directory(app.static_folder, path)
+    return send_file(f'../src/{path}')
 
 @app.route('/img/<path:path>')
 def serve_img(path):
-    return send_from_directory('../img', path)
+    return send_file(f'../img/{path}')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
