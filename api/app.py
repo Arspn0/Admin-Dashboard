a4931d8fb2d129879c43207893b6ad81d95b9c7c
ArisSpn_9 <arissopian0909@gmail.com>
2025-01-19 04:09:11 +0700

    add and config

---
+++ api/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='../src')
+
+@app.route('/')
+def serve_index():
+    return send_from_directory(app.static_folder, 'dashboard.html')
+
+@app.route('/src/<path:path>')
+def serve_src(path):
+    return send_from_directory(app.static_folder, path)
+
+if __name__ == "__main__":
+    app.run(debug=True)
